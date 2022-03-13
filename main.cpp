556746780b335032c6a6ff9863e0d2b21899df25
怪叔叔 <191462660@qq.com>
2022-03-13 22:50:23 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <glew.h>
+
+#define GLEW_STATIC
+#include <glfw3.h>
+#include <iostream>
+using namespace std;
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //OPengGL GLFW Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        printf("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    //Init GLEW
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK)
+    {
+        printf("Failed to Init GLFW");
+        glfwTerminate();
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+
+    glfwTerminate();
+    return 0;
+
+
+}
